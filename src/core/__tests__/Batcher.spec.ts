def9eb17dfb71e878cbe071bf0d04684aaa15d6b
Junmin Liu <junminliu@gmail.com>
2020-06-10 01:08:03 -0500

    feat: Merge `registerAtom`, `registerAsyncAtom` with `setAtom`

--- src/core/__tests__/Batcher.spec.ts
+++ src/core/__tests__/Batcher.spec.ts
@@ -1,34 +1,33 @@
 import Batcher from '../Batcher'
 
+const nextTick = () => new Promise(resolve => setImmediate(resolve))
+
 test('Can create a Batcher instance', () => {
   const mockNotifyChanges = jest.fn()
   const batcher = new Batcher(mockNotifyChanges)
   expect(batcher).not.toBeNull()
 })
 
-test('Batcher calls notifyChanges', () => {
+test('Batcher calls notifyChanges', async () => {
   const mockNotifyChanges = jest.fn()
   const batcher = new Batcher(mockNotifyChanges)
   batcher.notifyChange('foo')
   expect(mockNotifyChanges.mock.calls.length).toBe(0)
-  setTimeout(() => {
-    // notify changes in next run loop
-    expect(mockNotifyChanges.mock.calls.length).toBe(1)
-  })
+  await nextTick()
+  expect(mockNotifyChanges.mock.calls.length).toBe(1)
 })
 
-test('Batcher calls notifyChanges twice in two runloops', () => {
+test('Batcher calls notifyChanges twice in two runloops', async () => {
   const mockNotifyChanges = jest.fn()
   const batcher = new Batcher(mockNotifyChanges)
   batcher.notifyChange('foo')
   batcher.notifyChange('bar')
-  setTimeout(() => {
-    batcher.notifyChange('foo')
-    batcher.notifyChange('bar')
-    setTimeout(() => {
-      expect(mockNotifyChanges.mock.calls.length).toBe(2)
-    })
-  })
+
+  await nextTick()
+  batcher.notifyChange('foo')
+  batcher.notifyChange('bar')
+  await nextTick()
+  expect(mockNotifyChanges.mock.calls.length).toBe(2)
 })
 
 test('Batcher calls dispatchQueue', () => {
@@ -38,7 +37,7 @@ test('Batcher calls dispatchQueue', () => {
   expect(mockNotifyChanges.mock.calls.length).toBe(1)
 })
 
-test('Batcher can notify change by key', () => {
+test('Batcher can notify change by key', async () => {
   const mockListener = jest.fn()
   function notifyChanges (keys: Set<any> | Array<any>): void {
     keys.forEach(mockListener)
@@ -52,14 +51,14 @@ test('Batcher can notify change by key', () => {
   // do not notify change immediately
   expect(mockListener.mock.calls.length).toBe(0)
   expect(batcher.keys.size).toBe(2)
-  setTimeout(() => {
-    // notify changes in next run loop
-    expect(batcher.keys.size).toBe(0)
-    expect(mockListener.mock.calls.length).toBe(2)
-  })
+
+  await nextTick()
+  // notify changes in next run loop
+  expect(batcher.keys.size).toBe(0)
+  expect(mockListener.mock.calls.length).toBe(2)
 })
 
-test('Batcher can notify change by key', () => {
+test('Batcher can notify change by key', async () => {
   const mockListener = jest.fn()
   function notifyChanges (keys: Set<any> | Array<any>): void {
     keys.forEach(mockListener)
@@ -69,11 +68,10 @@ test('Batcher can notify change by key', () => {
   batcher.notifyChange('foo')
   batcher.notifyChange('bar')
 
-  setTimeout(() => {
-    batcher.notifyChange('hello')
-    expect(batcher.keys.size).toBe(1)
-    setTimeout(() => {
-      expect(mockListener.mock.calls.length).toBe(3)
-    })
-  })
+  await nextTick()
+  batcher.notifyChange('hello')
+  expect(batcher.keys.size).toBe(1)
+  await nextTick()
+
+  expect(mockListener.mock.calls.length).toBe(3)
 })
